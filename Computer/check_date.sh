99d079d37cf975ffe63d62ac64e6f6ee133289da
marco0913 <120720464+marco0913@users.noreply.github.com>
2024-05-16 13:17:31 +0100

    dev

--- Computer/check_date.sh
+++ Computer/check_date.sh
@@ -2,6 +2,5 @@
 current_year=$(date +%Y)
 if [ "$current_year" -eq 2019 ]; then
     echo "Incorrect date detected, initiating restart..."
-    systemctl restart systemd-timesyncd
     exit 1
 fi
--- Computer/setup.sh
+++ Computer/setup.sh
@@ -1,30 +1,18 @@
 #!/bin/bash
-
-# Ask for the hostname number and set the hostname
 read -p "Enter the number for the hostname (n in cage1x000{n}): " n
 if (( n < 10 )); then
     sudo hostnamectl set-hostname cage1x000$n
 else
     sudo hostnamectl set-hostname cage1x00$n
 fi
-
-# Refresh the hostname for the current session to avoid resolution errors
 sudo hostname $(hostnamectl --static)
-
-# Update and install pip if not installed
+sudo apt-get install libnss-mdns -y
 sudo apt-get update
 sudo apt-get install -y python3-pip
-
-# Install Python packages from the existing requirements.txt
 sudo pip3 install -r ./requirements.txt
-
-# Make sure check_date.sh is executable
 sudo chmod +x ./check_date.sh
-
-# Place the existing webapp.service in the correct directory and set it up
 sudo cp ./webapp.service /etc/systemd/system/webapp.service
 sudo systemctl daemon-reload
 sudo systemctl enable webapp.service
 sudo systemctl start webapp.service
-
 echo "Setup complete."
