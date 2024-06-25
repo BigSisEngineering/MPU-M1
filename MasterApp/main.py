275cbb09944798d9f02ebeda299522efd3a7bf19
djwtan <131467022+djwtan@users.noreply.github.com>
2024-06-25 09:59:56 +0100

    dev

--- MasterApp/main.py
+++ MasterApp/main.py
@@ -1,20 +1,29 @@
-import threading
-
-# ------------------------------------------------------------------------------------------------ #
 from src import http_server, tasks
-from src.http_server import post_handler
+
+# ------------------------------------------------------------------------------------ #
+from src._shared_variables import SV
+
+# ------------------------------------------------------------------------------------ #
+from src import CLI
+from src.CLI import Level
+
+print_name = "MAIN"
+
+
+def terminate_program():
+    SV.KILLER_EVENT.set()
+    CLI.printline(Level.ERROR, "({:^15}) Program Terminated.".format(print_name))
 
 
 def main():
+    try:
+        tasks.start()
+        http_server.start()
+        terminate_program()
 
-    tasks.start()
-    http_server.start()
+    except KeyboardInterrupt:
+        terminate_program()
 
 
 if __name__ == "__main__":
     main()
-
-    # ------------------------------------------------------------------------------------ #
-    # from src.data.a2 import debug
-
-    # debug()
